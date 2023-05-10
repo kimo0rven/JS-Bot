10ea0637950771ad21c71949b826c25be49e5cd1
Twigs <kimorvenvalencia@gmail.com>
2023-05-10 19:23:47 +0800

    Added a testing code

--- src/commands/misc/excel.js
+++ src/commands/misc/excel.js
@@ -14,27 +14,34 @@ module.exports = {
 		await interaction.deferReply();
 
 		const reply = await interaction.fetchReply();
+		var accessTokenResponse = null
 
-			
-		var headers = {
-			'Authorization': 'Zoho-oauthtoken 1004.9fb5c2ca9ef5d2edbfc04f779d07a9dd.02483ff3b2f8699081e287ca57a10878'
-		};
-		axios.post('https://sheet.zoho.com/api/v2/15vy0d864d94014f7412aacaded4cd0da1ce7?method=worksheet.list', null, {headers})
-		.then(response => {
-			console.log(response.data);
-		})
-		.catch(function (error) {
-			console.log("ERROR NI SIYA")
-			axios.post("https://accounts.zoho.com/oauth/v3/device/code?client_id=1004.BEUNCBTR7QRSMC697A7OAWEYC119WD&scope=ZohoSheet.dataAPI.UPDATE,ZohoSheet.dataAPI.READ&grant_type=device_request&access_type=offline&")
-			.then(response => {
-			console.log(response.data);
-		})
-			.catch(function (error) {
-			
-		});
-			// console.error(error);
-		});
-	interaction.editReply("test")
+		const url = "https://sheet.zoho.com/api/v2/15vy0d864d94014f7412aacaded4cd0da1ce7?method=worksheet.insert&worksheet_name=test 3"
+
+		const headers = {
+			'Authorization': 'Zoho-oauthtoken ' + process.env.access_token 
+		}
+
+		axios.post("https://accounts.zoho.com/oauth/v3/device/setCode?devcode=T1RUTi05MTg2")
+			.then( response => {
+				console.log(response.data)
+			})
+			.catch ( error => {
+				console.log(error)
+			})
+		// axios.post(url, null, {headers})
+		// 	.then( response => {
+		// 		accessTokenResponse = response.data
+		// 		console.log(response.data)
+		// 	})
+		// 	.catch(error => {
+		// 		console.log(error)
+		// 		axios.post("https://accounts.zoho.com/oauth/v3/device/token?client_id=1004.2UHN26P51ESYSZY3YT57MBIRUJCFBH&grant_type=device_token&client_secret=7f11e3ffe3cbcbca567555bd35acad03c3d9bda8a2&code=1004.dee0a2971c3bc4407e1c50c782cca58c.02f36a148a68f6db873a8e680fe6cad2")
+		// 			.then( response => {
+		// 				console.log(response)
+		// 			})
+		// 	})
+	interaction.editReply(process.env.refresh_token)
 	},
 }
 
